cd945a29a050a70f569beeeed5b3451b747d8342
Ahmet Hulusi Akan <95524111+akanahmethulusi@users.noreply.github.com>
2025-04-30 12:38:45 +0200

    Add files via upload
    db

---
+++ autos.sql
@@ -0,0 +1,11 @@
+67a2eae9-8da6-4931-a227-11b85647ac6a,Rorschacherberg,2025,XC40,Volvo,http://localhost:8080/autos/image/67a2eae9-8da6-4931-a227-11b85647ac6a.jpeg,43.000,aktiv,gebracuht,769-099-31
+b04714e8-8d4a-4c14-bf36-53c05faf8620,Thal,2021,XC60,Volvo,http://localhost:8080/autos/image/b04714e8-8d4a-4c14-bf36-53c05faf8620.png,19000,active,gebraucht,82932777
+5bd65413-dc5c-42c4-a2bd-662a4e47d778,Bern,2021,XC90,Volvo,http://localhost:8080/autos/image/5bd65413-dc5c-42c4-a2bd-662a4e47d778.png,17800,active,gebraucht,3894477
+5c4d480f-3260-4d12-94c3-f81430932978,CH-9400-Rorschach,2020,HR-V,Honda,http://localhost:8080/autos/image/5c4d480f-3260-4d12-94c3-f81430932978.png,CHF 3'900.-,Active,Gebraucht,(792) 909 01
+bac1edf5-5375-4228-aa44-36add044dba3,CH-9425-Thal,2024,CR-V,Honda,http://localhost:8080/autos/image/bac1edf5-5375-4228-aa44-36add044dba3.jpeg,CHF 34'000.-,Active,Gebraucht,(798) 088 23
+bca310cf-6f54-44f5-ae98-bbdc3e518acc,CH-8000-Zürich,2021,Accent,Hyundai,http://localhost:8080/autos/image/bca310cf-6f54-44f5-ae98-bbdc3e518acc.png,CHF 7'000.-,Active,Gebraucht,(798) 277 29
+9d20f9b2-0e8b-4770-897e-18d03bc0a655,CH-9543-St.Margarethen,2022,Cilo,Renault,http://localhost:8080/autos/image/9d20f9b2-0e8b-4770-897e-18d03bc0a655.jpeg,CHF 5'400.-,Active,Gebraucht,(790) 229 39
+7768866b-f7f7-4162-aeb9-12adfa560303,CH-9543-St.Margarethen,2020,Granada,Seat,http://localhost:8080/autos/image/7768866b-f7f7-4162-aeb9-12adfa560303.png,CHF 6'700.-,Active,Gebraucht,(798) 389 97
+bc119c75-112c-47fd-8163-c9a4edfff481,CH-9404-Rorschacherberg,2024,Model 3,Tesla,http://localhost:8080/autos/image/bc119c75-112c-47fd-8163-c9a4edfff481.png,CHF 11'000.-,Active,Gebraucht,(783) 999 20
+ef71e36b-ac13-4c88-8f7b-ddc60e6ef69a,CH-9400-Rorschach,2024,Corolla,Toyota,http://localhost:8080/autos/image/ef71e36b-ac13-4c88-8f7b-ddc60e6ef69a.jpeg,CHF 33'000.-,Active,Gebraucht,(736) 200 12
+bac6833f-4258-4869-aef4-ebbd93d59d13,CH-9000-St. Gallen,2021,Karoq,Skoda,http://localhost:8080/autos/image/bac6833f-4258-4869-aef4-ebbd93d59d13.jpeg,CHF 7'000.-,Active,Gebraucht,(703) 999 48
